096b3cfb258d81342a11de37c0a2543d777af6a0
Fabio Motta <fabiomotta0311@gmail.com>
2020-05-27 04:00:04 -0300

    list products wip

--- src/pages/main.js
+++ src/pages/main.js
@@ -6,18 +6,27 @@ export default class Main extends Component {
     static navigationOptions = {
         title: "JSHunt"
     };
+
+    state = {
+
+    };
     
     componentDidMount(){
         this.loadProducts();
     }
 
-    loadProducts = () => {
-        
+    loadProducts = async () => {
+        const response = await api.get('/products')
+        const { docs } = response.data;
+
     };
     render (){
         return (
             <View>
                 <Text>Página Main</Text>
+                {this.state.docs.map(product => (
+                    <Text key={product._id}>{product.title }</Text>
+                ))}
             </View>
         );
     }
